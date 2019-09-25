14554997e47642e99e3eff7e1ea06b70683b534b
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2019-09-25 00:55:53 -0500

    Access path from individual posts to Author's profile page has been fixed

--- front/src/app/modules/posts/post-autor-info.component.ts
+++ front/src/app/modules/posts/post-autor-info.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, SimpleChanges, OnChanges} from '@angular/core';
 import { AuthorInfoModel } from 'src/app/models/authorInfo.model';
+import {FollowingService} from "../../services/following.service";
 
 @Component({
   selector: 'app-post-autor-info',
   templateUrl: './post-autor-info.component.html',
   styles: []
 })
-export class PostAutorInfoComponent implements OnInit {
+export class PostAutorInfoComponent implements OnInit, OnChanges {
+  public followBtnName = 'unchanged';
 
   @Input() authorInfo: AuthorInfoModel;
-  constructor() {
+  constructor(private followingService: FollowingService) {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.updateFollowButtonName(this.followingService.isFollowing(this.authorInfo._id));
   }
 
+
+  updateFollowButtonName(isFollowing: boolean) {
+    if (isFollowing) {
+      this.followBtnName = 'Unfollow';
+    } else {
+      this.followBtnName = 'Follow';
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateFollowButtonName(this.followingService.isFollowing(this.authorInfo._id));
+  }
+  followUnfollow() {
+    if (this.followingService.isFollowing(this.authorInfo._id)) {
+      this.updateFollowButtonName(false);
+      this.followingService.unfollow(this.authorInfo._id);
+    } else {
+      this.updateFollowButtonName(true);
+      this.followingService.follow(this.authorInfo._id);
+    }
+  }
 }
